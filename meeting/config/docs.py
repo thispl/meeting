4dc20d406c3e84e7835b726f860219cc9b81c68a
root <root@e2e-115-19.ssdcloudindia.net>
2024-01-28 12:15:09 +0530

    updated meeting

---
+++ meeting/config/docs.py
@@ -0,0 +1,11 @@
+"""
+Configuration for docs
+"""
+
+source_link = "https://github.com/frappe/meeting"
+docs_base_url = "https://frappe.github.io/meeting"
+headline = "Plan and Record Meetings"
+sub_heading = "Prepare agenda, invite users and record minutes of a meeting"
+
+def get_context(context):
+	context.brand_html = "Meeting"
